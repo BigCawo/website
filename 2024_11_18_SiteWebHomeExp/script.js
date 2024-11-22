7447f1e866dcefc0935f1bad307782a8cac75b7d
Bernaud Eliot <bernaud.eliot@gmail.com>
2024-11-22 19:17:48 +0100

    2024_11_22_test yeahh bb

---
+++ 2024_11_18_SiteWebHomeExp/script.js
@@ -0,0 +1,7 @@
+function oui(){
+    document.getElementById("introText").style.display ="none"
+        document.getElementById("genButton").style.display ="none"
+    document.getElementsByClassName("animatedImage")[0].style.display ="block"
+    document.getElementsByClassName("animatedImage2")[0].style.display ="block"
+    document.getElementsByClassName("animatedImage3")[0].style.display ="block"
+}
