4054b31422cdc7a2ef0dc38e543b2003b7d77cb9
Bernaud Eliot <bernaud.eliot@gmail.com>
2024-12-15 12:21:42 +0100

    2024_12_15

--- 2024_11_18_SiteWebHomeExp/global.js
+++ 2024_11_18_SiteWebHomeExp/global.js
@@ -1,5 +1,7 @@
 let Mq480 = window.matchMedia("(max-width: 480px)")
-
+if (localStorage.getItem("explored") == 1){
+    document.body.style.animation = "none"
+ }
 if(performance.navigation.type == 2){
     location.reload(true);
  }
