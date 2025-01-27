c51d787958c251fe491a32eb09fdb37a26d3179f
Bernaud Eliot <bernaud.eliot@gmail.com>
2025-01-27 15:57:37 +0100

    2025_01_27

--- scripts/scriptServices.js
+++ scripts/scriptServices.js
@@ -1,8 +1,11 @@
-let mainContainer = document.getElementById("mainContainer")
-let textContainer = document.getElementById("textContainer")
-let imgContainer = document.getElementById("imgContainer")
-let textNbr = textContainer.getElementsByClassName("text")
+const mainContainer = document.getElementById("mainContainer")
+const textContainer = document.getElementById("textContainer")
+const imgContainer = document.getElementById("imgContainer")
+const textNbr = textContainer.getElementsByClassName("text")
+const carouselTriggers = document.getElementsByClassName("carouselTrigger")
+const carouselGroup = document.getElementsByClassName("carouselContainer")[0].getElementsByTagName("div")
 bonjour = "0"
+
 
 // console.log(mainContainer.offsetHeight, textContainer.offsetHeight,textNbr.length)
 // console.log(imgContainer.scrollLeft)
@@ -21,3 +24,20 @@ function imgContainerMovement(){
     bonjour = Math.ceil((textContainer.scrollTop - 10) / textContainer.offsetHeight)
     imgContainer.scrollLeft = textContainer.scrollTop * (imgContainer.offsetWidth/textContainer.offsetHeight)
 }
+// console.log(carouselGroup.length,carouselTriggers.length)
+
+// for (i = 0; i < carouselTriggers.length ; i++){
+//     carouselTriggers[i].style.backgroundColor = "blue"
+//     if (Mq480.matches == false){
+  
+//         carouselTriggers[i].addEventListener("mousemove",function(){ 
+
+//         for (g = 0; g < carouselTriggers.length ; g ++) {     
+//             carouselGroup[g].style.display = "none"      
+//         } 
+//         console.log(i)
+//         })
+
+         
+// }
+// }
